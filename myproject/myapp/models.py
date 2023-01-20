d35ed1537eabffd3c9ede030d9ff553e4b7d385f
Kristina Pashchenko <kristinapashchenko@Kristinas-MacBook-Pro.local>
2023-01-20 14:45:48 +0100

    models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
+class Drinks(models.Model):
+    drink_name = models.CharField(max_length=200)
+    price = models.IntegerField()
