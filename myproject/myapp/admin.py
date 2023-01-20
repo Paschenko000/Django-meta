72e2b554d032f0eab535efd400afb049aebe7fc3
Kristina Pashchenko <kristinapashchenko@Kristinas-MacBook-Pro.local>
2023-01-20 15:53:31 +0100

    not full version

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Menu, MenuCategory
 
 # Register your models here.
+# Register the models
+#admin.site.register(Menu)
+#admin.site.register(MenuCategory)
