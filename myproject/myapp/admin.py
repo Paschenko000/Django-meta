8fd164459e2457726f631ac6efb2adf4f47bf291
Kristina Pashchenko <kristinapashchenko@Kristinas-MacBook-Pro.local>
2023-01-21 13:16:31 +0100

    foreign key

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -3,5 +3,6 @@ from .models import Menu, MenuCategory
 
 # Register your models here.
 # Register the models
-#admin.site.register(Menu)
-#admin.site.register(MenuCategory)
+admin.site.register(Menu)
+admin.site.register(MenuCategory)
+
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*%q%inswkcwuwxcp+#r^^2n_z-!rjd)7_py3gfpbo(8_telb!'
+SECRET_KEY = 'django-insecure-4ok^nbk1crv00wuf&+!v6+3e-uc63t=#51dfv^$0^dj&88*6jd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myapp.apps.MyappConfig'
+    'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
